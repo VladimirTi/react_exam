da2fd299d38a5de40daa1f20171c2a2e8c83f27d
VladimirTi <tytmes@mail.ru>
2019-06-24 01:10:10 +0300

    Updates

---
+++ precache-manifest.c8f0ba1f09e5ce87da0d5316e56df737.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a703bfd062c5d4e8b74f8eaa615d3919",
+    "url": "/react_exam/index.html"
+  },
+  {
+    "revision": "38c3a83871da0dff1b15",
+    "url": "/react_exam/static/css/main.6bccffa8.chunk.css"
+  },
+  {
+    "revision": "397181ca8fd0d95467f7",
+    "url": "/react_exam/static/js/2.3bf70ace.chunk.js"
+  },
+  {
+    "revision": "38c3a83871da0dff1b15",
+    "url": "/react_exam/static/js/main.9ab24848.chunk.js"
+  },
+  {
+    "revision": "4c3dcd7e4ba5e5cedf14",
+    "url": "/react_exam/static/js/runtime~main.4b58bd7d.js"
+  }
+]);
